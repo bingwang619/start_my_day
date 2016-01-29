ed6fd318121631d999e5041edf4b95fc1ce13f32
BingWang <bingwang619@gmail.com>
2016-01-29 12:02:54 +0800

    add question time block

--- start_my_day.py
+++ start_my_day.py
@@ -19,7 +19,8 @@ def load_config(config_file):
             sec_opt2val[section][option] = config.get(section,option)
     return sec_opt2val
 
-def load_template(template_file):
+def load_template(conf):
+    template_file = conf["base_config"]["diary_template"]
     temp_block = []
     handle = open(template_file)
     line = handle.readline()
@@ -33,18 +34,44 @@ def load_template(template_file):
         line = handle.readline()
     return "".join(temp_block)
 
-def get_date_today(time_str):
+def get_date_today(conf):
+    time_str = conf["formatter"]["date_today"]
     # time_str = "{0:%Y}年{0:%m}月{0:%d}日 {0:%H}时{0:%M}分"
     return time_str.format(datetime.today())
 
-def get_today_habit(habit_list):
-    return "".join(random.sample(habit_list,1))
+def get_today_habit(conf):
+    habit_list = conf["habit_list"].keys()
+    habit_count = 1
+    return "".join(["+ %s\n"%(s) for s in 
+        random.sample(habit_list,habit_count)])
+
+def get_reminder_today(conf):
+    return "day"
+
+def get_book_to_read(conf):
+    book_list = conf["book_list"]
+    note_template = conf["base_config"]["note_template"]
+
+def get_questions(conf):
+    questions_list = conf["questions_list"].keys()
+    questions_count = min(len(questions_list),
+            conf["base_config"]["questions_per_day"])
+    return "\n".join(["+ %s\n"%(s) for s in 
+        random.sample(questions_list,questions_count)])
+
+def get_whether(conf):
+    return "today_whether"
 
 def main(config_file):
     conf = load_config(config_file)
-    template_string = load_template(conf["base_config"]["diary_template"])
-    data = {"date_today":get_date_today(conf["formatter"]["date_today"]),
-            "habit_today":get_today_habit(conf["habit_list"].keys())}
+    template_string = load_template(conf)
+    data = {"date_today":get_date_today(conf),
+            "habit_today":get_today_habit(conf),
+            "reminder_today":get_reminder_today(conf),
+            "book_to_read_today":get_book_to_read(conf),
+            "questions_today":get_questions(conf),
+            "whether_today":get_whether(conf)
+            }
     print template_string.format(**data)
 
 if __name__ == "__main__":
